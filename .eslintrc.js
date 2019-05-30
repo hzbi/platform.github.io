09eb415993fb28502b6a340e822a1dc66145f654
bihaoze <haoze.bi@hosjoy.com>
2019-05-30 14:43:30 +0800

    eslint修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,52 @@
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: 'standard',
-  globals: {
-    __static: true
-  },
-  plugins: [
-    'html'
-  ],
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module',
+        parser: 'babel-eslint'
+    },
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: 'standard',
+    globals: {
+        __static: true
+    },
+    plugins: [
+        'html'
+    ],
+    rules: {
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        // allow async-await
+        'generator-star-spacing': 0,
+        // allow debugger during development
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        "indent": ["error", 4, {
+            "SwitchCase": 1,
+            "VariableDeclarator": 1,
+            "outerIIFEBody": 1,
+            "MemberExpression": 1,
+            "FunctionDeclaration": {
+                "parameters": 1,
+                "body": 1
+            },
+            "FunctionExpression": {
+                "parameters": 1,
+                "body": 1
+            },
+            "CallExpression": {
+                "arguments": 1
+            },
+            "ArrayExpression": 1,
+            "ObjectExpression": 1,
+            "ImportDeclaration": 1,
+            "flatTernaryExpressions": false,
+            "ignoreComments": false
+        }],
+        'eol-last': 'off',
+        "eqeqeq": 0
+    }
 }
